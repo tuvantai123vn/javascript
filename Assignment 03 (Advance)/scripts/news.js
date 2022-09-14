95d456bb295b7ba4d1cbec073a6f45823ce78c5b
tuvantai123vn <nguyenthanhtu.cdct@gmail.com>
2022-09-14 14:49:56 +0700

    test_13_09

--- Assignment 03 (Advance)/scripts/news.js
+++ Assignment 03 (Advance)/scripts/news.js
@@ -1,5 +1,5 @@
 'use strict'
-if(currentUser){
+if (currentUser) {
     const newsContainer = document.getElementById('news-container');
     const btnPrev = document.getElementById('btn-prev');
     const pageNum = document.getElementById('page-num');
@@ -7,50 +7,51 @@ if(currentUser){
     getDatanews('us', 1)
     let totalResults = 0;
     // lấy dữ liệu Data news thông qua API
-    async function getDatanews(country, page){
-        try{
+    async function getDatanews(country, page) {
+        try {
             // kết nối và lấy dữ liệu API
             const res = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${currentUser.category}&pageSize=${currentUser.pageSize}&page=${page}&apiKey=8b41ada840524216b0d768c1f61398b6`)
             const data = await res.json();
             console.log(data);
             displayNewList(data);
-            
-        }catch(err){
+
+
+        } catch (err) {
             alert('ERROR: Không thể truy cập')
         }
     }
     // kiểm tra nếu textcontent = 1 thì ẩn btnPrev
-    function checkBtnPrev(){
-        if(pageNum.textContent == 1){
+    function checkBtnPrev() {
+        if (pageNum.textContent == 1) {
             btnPrev.style.display = 'none';
-        }else{
+        } else {
             btnPrev.style.display = 'block';
         }
     }
     // kiểm tra nếu textcontent bằng số lượng bài viết chia cho pageSize thì ẩn btnNext
-    function checkBtnNext(){
-        if(pageNum.textContent == Math.ceil(totalResults / currentUser.pageSize)){
-        btnNext.style.display = 'none';
-        }else{
+    function checkBtnNext() {
+        if (pageNum.textContent == Math.ceil(totalResults / currentUser.pageSize)) {
+            btnNext.style.display = 'none';
+        } else {
             btnNext.style.display = 'block';
         }
     }
     // khi người dùng nhấn vào nút btnPrev thì lùi lại 1 trang
-    btnPrev.addEventListener('click', function(){
+    btnPrev.addEventListener('click', function () {
         getDatanews('us', --pageNum.textContent);
     })
     // khi người dùng nhấn vào nút btnNext thì tăng lên 1 trang
-    btnNext.addEventListener('click', function(){
+    btnNext.addEventListener('click', function () {
         getDatanews('us', ++pageNum.textContent);
     })
     // hàm này dùng để render các bài viết ra màn hình 
-    function displayNewList(data){
+    function displayNewList(data) {
         totalResults = data.totalResults;
         checkBtnPrev();
         checkBtnNext();
         let html = '';
-        data.articles.forEach(function(articles){
-            html +=`
+        data.articles.forEach(function (articles) {
+            html += `
             <div id="news-container">
                     <div class="card flex-row flex-wrap">
                         <div class="card mb-3" style="">
@@ -74,7 +75,7 @@ if(currentUser){
         });
         newsContainer.innerHTML = html;
     }
-}else{
+} else {
     alert('Vui lòng đăng nhập hoặc đăng ký nếu chưa có tài khoản');
     window.location.href = '../index.html';
 }
